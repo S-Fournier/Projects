e17bd8204ec54bd22ba9241f721b96e9959424dd
Silvia <94818168+S-Fournier@users.noreply.github.com>
2022-08-22 15:22:28 -0400

    Update test.py

--- Other Projects/test.py
+++ Other Projects/test.py
@@ -8,15 +8,18 @@ import csv
 
 html_text=requests.get('https://en.wikipedia.org/wiki/Lists_of_cities_by_country').text
 soup=BeautifulSoup(html_text,'lxml')
-Countries_Raw=soup.find_all('a')
+Countries_Raw=soup.find_all('b')
 Countries=np.array([])
+Country_Links=np.array([])
 filter='List of cities in '
 for i in(range(len(Countries_Raw))):
     Countries_Refined=Countries_Raw[i].get_text()
     if(len(Countries_Refined)>=len(filter)):
       if(Countries_Refined[:len(filter)]==filter):
+        Country_Links=np.append(Country_Links,Countries_Refined)
         Countries_Refined=Countries_Refined[len(filter):]
         Countries=np.append(Countries,Countries_Refined)
 
-
-print(Countries)
+for i in(range(len(Country_Links))):
+  Country_Links[i]=Country_Links[i].replace(" ","_")
+print(Country_Links)
