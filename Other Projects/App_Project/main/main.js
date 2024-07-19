81beef3fc25af919665bb6c802ea1b49109e7876
Silvia <94818168+S-Fournier@users.noreply.github.com>
2024-07-19 06:43:45 -0400

    Add files via upload

--- Other Projects/App_Project/main/main.js
+++ Other Projects/App_Project/main/main.js
@@ -1,68 +1,141 @@
-var to_do_activities = [];
+var options_data = localStorage.getItem('options_data');
+var options_data = JSON.parse(options_data);
+var default_activity_list = options_data.default_activity_list;
+//var frequency = Number(options_data.frequency)*60000;
+var frequency = 3000;
+var stop = options_data.stop;
+var save_info = {};
+var stats = {};
 var activities = [];
 var check = [];
-var break_times = [];
-var main_obj = {};
-var next = false;
+var score = [];
+var score_max = 0;
 
-function add_to_do_list() {
-    var to_do_activiy = document.getElementById("to_do_activity").value;
-    to_do_activities.push(to_do_activiy);
-    document.getElementById("to_do_activity")="";
-    input_display("display_to_do_list", "div", to_do_activities);
+for(let i = 0; i < default_activity_list.length; i++){
+    activities.push(default_activity_list[i]);
+    check.push(false);
+    score.push(0);
+}
+
+interval = setInterval(timer,60000);
+
+
+function timer() {
+    var clock = new Date();
+    clock = clock.getHours() + ":" + clock.getMinutes();
+    if(clock >= stop){
+        clearTimeout(timeout);
+        clearInterval(interval);
+        reset();
+        end();
+    }
 }
 
 function start() {
-    document.getElementById("frame").innerHTML="";
-    for (let i=0; i<default_activity_list.length; i++) {
-        activities.push(default_activity_list[i]);
-    }
-    for (let i=0; i<to_do_activities; i++) {
-        activities.push(to_do_activities[i]);
-    }
-    for (let i=0; i<activities; i++) {
-        check.push(false)
-    }
-    setTimeout(display_checkpoint,frequency)
+    document.getElementById("check").style.display = "none";
+    document.getElementById("initial").style.display = "none";
+    timeout = setTimeout(display_checkpoint, frequency);
+    
 }
 
-function checkpoint() {
-    let checkpoint_time = Date.now;
-    break_times.push(checkpoint_time);
-    main_obj[checkpoint_time] = {};
-    for(let i=0; i<activities.length; i++){
-        
-    }
-
-    display_input_data()
-    
-    if(checkpoint_time>=stop){
-        end_day();
-    }
-    else {        
-    }
+function reset() {
+    document.getElementById("checklist_ul").innerHTML = "";
+    check = [];
 }
 
 function display_checkpoint() {
-    var target_ID = document.getElementById("check");
-    target_ID.innerHTML = "";
+    document.getElementById("check").style.display = "block";
+    let ul = document.getElementById("checklist_ul");
 
     for (let i = 0; i < activities.length; i++) {
-        var new_checkbox = document.createElement("input");
-        new_checkbox.type = "checkbox";
-        new_checkbox.id = activities[i]
-        target_ID.appendChild(new_checkbox);
-    }
+        let li = document.createElement("li");
+        let li_text = document.createTextNode(activities[i]);
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.id = activities[i];
 
-    var submit_button = document.createElement("button");
-    submit_button.innerHTML = "Submit";
-    submit_button.setAttribute("onclick","submit()");
-    target_ID.appendChild(submit_button);
+        ul.appendChild(li);
+        li.appendChild(li_text);
+        li.appendChild(checkbox);
+    }
 }
 
 function submit() {
-    for (let i = 0; i < check.length; i++) {
-        var x = document.getElementById(test[i]);
+
+    for (let i = 0; i < activities.length; i++) {
+        let x = document.getElementById(activities[i]);
         check[i] = x.checked;
+        if(x == true){
+            score[i] = score[i] + 1;
+        }
     }
+
+    score_max = score_max + 1;
+    
+    let date = new Date();
+    let time = date.getHours() + ":" + date.getMinutes();
+    
+    save_info[time] = {};
+    
+    for (let i = 0; i < activities.length; i++){
+        save_info[time][activities[i]] = check[i];
+    }
+
+    console.log(save_info);
+
+    reset();
+    start();
+}
+
+function add_to_list() {
+
+    let activity = document.getElementById("to_do_activity").value;
+    let li = document.createElement("li");
+    let li_text = document.createTextNode(activity);
+    let span = document.createElement("SPAN");
+    let span_text = document.createTextNode("\u00D7");
+
+    span.appendChild(span_text);
+    li.appendChild(li_text);
+    li.appendChild(span);
+
+    activities.push(activity);
+    check.push(false);
+    score.push(0);
+
+    document.getElementById("display_list").appendChild(li);
+    document.getElementById("to_do_activity").value = "";
+
+    span.addEventListener("click", function() {
+        let li = this.parentElement
+        let i = activities.indexOf(input_value);
+        li.style.display = "none";
+        activities.splice(i,1);
+        check.splice(i, 1);
+        score.splice(i, 1);
+    });
+}
+
+
+
+function end() {
+    for(let i = 0; i < activities.length; i++) {
+        let li = document.createElement("li");
+        let li_text = document.createTextNode(activities[i]);
+        let span = document.createElement("span");
+        let x = score[i] / score_max * 100
+        let span_text = document.createTextNode(x + "%")
+
+        li.appendChild(li_text);
+        li.appendChild(span);
+        li.appendChild(span_text);
+
+        document.getElementById("check").appendChild(li);       
+    }
+    let day = new Date();
+    day = day.getFullYear() + "-" + day.getMonth() + "-" + day.getDay();
+
+    stats[day] = save_info;
+
+    localStorage.setItem('stats',JSON.stringify(stats));
 }
--- Other Projects/App_Project/options/options.js
+++ Other Projects/App_Project/options/options.js
@@ -1,41 +1,48 @@
 var default_activity_list = [];
-var frequency;
-var stop;
-var close = document.getElementsByClassName("close");
+var options_data = localStorage.getItem('options_data');
 
-function save_interval_time() {
-    var frequency = document.getElementById("interval_time").value;
-    return frequency
-}
-
-function save_end_time() {
-    var stop = document.getElementById("end_time").value;
-    return stop
-}
-
-function add_to_list() {
-    
-    var default_activity = document.getElementById("default_activity").value;
-    default_activity_list.push(default_activity);
-
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("default_activity").value;
-    var activity = document.createTextNode(inputValue);
-    li.appendChild(activity);
-    document.getElementById("display_list").appendChild(li);
-    document.getElementById("default_activity").value = "";
-
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-  
-    for (let i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            var div = this.parentElement;
-            div.style.display = "none";
-            default_activity_list.splice(i, 1);
-        }
+if(options_data !== null){
+    var options_data = JSON.parse(options_data);
+    var default_activity_list = options_data.default_activity_list;
+    var frequency = Number(options_data.frequency)*60000;
+    var stop = options_data.stop;
+    for(let i = 0; i < default_activity_list.length; i++){
+        let default_activity = default_activity_list[i];
+        add_to_list(default_activity);
     }
+}
+
+function add_to_array() {
+    let default_activity = document.getElementById("default_activity").value; //get value from target element   
+    default_activity_list.push(default_activity); //append value into array for storage
+    add_to_list(default_activity); //pass value to add_to_list function
+}
+
+function add_to_list(input_value) {
+      
+    let li = document.createElement("li"); //create a child li
+    let li_text = document.createTextNode(input_value); //create text from input_value
+    let span = document.createElement("SPAN"); //create close element
+    let span_text = document.createTextNode("\u00D7"); //create x button
+
+    span.appendChild(span_text);
+    li.appendChild(li_text);
+    li.appendChild(span);
+
+    document.getElementById("display_list").appendChild(li); //append child li to parent ul
+    document.getElementById("default_activity").value = ""; //delete target element's value
+    
+    span.addEventListener("click", function() {
+        let li = this.parentElement
+        let i = default_activity_list.indexOf(input_value);
+        li.style.display = "none";
+        default_activity_list.splice(i,1); 
+    });
+}
+
+function save_settings() {
+    var frequency = document.getElementById("interval_time").value;
+    var stop = document.getElementById("end_time").value;
+    var options_data = {'default_activity_list' : default_activity_list, 'frequency' : frequency, 'stop' : stop};
+    localStorage.setItem('options_data', JSON.stringify(options_data));
 }
